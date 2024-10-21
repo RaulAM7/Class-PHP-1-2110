0661cd1c3b10584ad0f10b57d9167900feac979c
RaulAM <raulartilesm@gmail.com>
2024-10-21 12:02:14 +0100

    Update

--- MyPHP.php
+++ MyPHP.php
@@ -5,6 +5,26 @@
     */
 
     //Para printear
-    echo 'Hello world';
+    echo 'Hello world'.PHP_EOL;
 
-?>
+    //Para definir variables
+    $myvariable = 'Soy una variable';
+    echo $myvariable.PHP_EOL;
+
+    define('MY_CONSTANT', 'My value.'); 
+
+    const MY_CONSTANT2 = 20;
+
+    echo MY_CONSTANT.PHP_EOL;
+    echo MY_CONSTANT2.PHP_EOL;
+
+    //Data Types
+
+    //1.- Strings
+    $mystring = 'Soy un string';
+    echo $mystring.PHP_EOL;
+
+    //2.- Integresers
+    $myINT = 30;
+    echo $myINT.PHP_EOL;
+
